2aeab5fa47cfa28bbfa58e7073aced2902adec90
fuzhou.chen <fuzhouch@gmail.com>
2026-02-16 01:52:40 +0800

    Create a WinRT component called Calculator.
    This change is to demonstrate how to build an WinRT component via CMake.
    It includes the use of midl.exe, cppwinrt.exe, and how to add
    implementation.

---
+++ component/calculator/Calculator.cpp
@@ -0,0 +1,8 @@
+#include "Calculator.h"
+
+using namespace winrt::WinAppTutorial::implementation;
+
+int32_t Calculator::Add(int32_t a, int32_t b)
+{
+    return a + b;
+}
---
+++ component/calculator/Calculator.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "WinAppTutorial/Calculator.g.h"
+
+namespace winrt::WinAppTutorial::implementation
+{
+    struct Calculator : CalculatorT<Calculator>
+    {
+        Calculator() = default;
+        int32_t Add(int32_t a, int32_t b);
+    };
+}
+namespace winrt::WinAppTutorial::factory_implementation
+{
+    struct Calculator : CalculatorT<Calculator, implementation::Calculator>
+    {
+    };
+}
