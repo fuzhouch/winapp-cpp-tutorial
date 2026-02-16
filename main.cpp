424c082367ce3947627627b5124816b915de0c01
fuzhou.chen <fuzhouch@gmail.com>
2026-02-16 22:49:28 +0800

    Let winapp-cpp-tutorial consume Calculator component via side-by-side.

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main() {
         int result = calculator.Add(3, 5);
         std::cout << "Result of 3 + 5 = " << result << std::endl;
     } catch (const winrt::hresult_error& ex) {
-        std::wcout << L"WinRT Error: " << ex.message().c_str() << std::endl;
+        std::wcout << L"WinRT Error: 0x" << std::hex <<ex.code() << std::dec << std::endl;
         return 1;
     } catch (const std::exception& e) {
         std::cout << "Error: " << e.what() << std::endl;
