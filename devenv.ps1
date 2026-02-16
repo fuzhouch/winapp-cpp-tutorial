b5d06af0afcb6345619bf18c2389f6c68acba9b5
fuzhou.chen <fuzhouch@gmail.com>
2026-02-16 23:25:30 +0800

    Update devenv.ps1 to configure arch and host-arch for building.

--- devenv.ps1
+++ devenv.ps1
@@ -1,7 +1,12 @@
+param(
+    $Arch = "amd64",
+    $HostArch="amd64"
+)
 $VSInstallPath = &"${ENV:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -property installationPath
 $DevShellDLL = "${VSInstallPath}\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
 Import-Module "${DevShellDLL}"
 
 ${CurrentWorkingDirectory} = Get-Location
-Enter-VsDevShell -VsInstallPath "${VSInstallPath}"
-Set-Location ${CurrentWorkingDirectory}
+Enter-VsDevShell -VsInstallPath "${VSInstallPath}" `
+    -StartInPath ${CurrentWorkingDirectory} `
+    -Arch $Arch -HostArch $HostArch
